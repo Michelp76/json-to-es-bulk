1c566548e28c33ba91618e44b8a51b1ea0c7da5d
michelp76 <michel.puissant@inextenso.fr>
2025-01-16 09:26:45 +0100

    first commit

--- index.js
+++ index.js
@@ -1,95 +1,108 @@
-var path = require('path'),
-    yargs = require('yargs'),
-    colors = require('colors/safe'),
-    util = require('util'),
-    fs = require('fs'),
-    _ = require('lodash')
-    isVerboseOn = false;
+var path = require("path"),
+  yargs = require("yargs"),
+  colors = require("colors/safe"),
+  util = require("util"),
+  fs = require("fs"),
+  _ = require("lodash");
+striptags = require("striptags");
+isVerboseOn = false;
 
-process.on('uncaughtException', function(err) {
-
-    if(isVerboseOn){
-        console.log(colors.red(err.stack));
-    }
-    else{
-        console.log(colors.red(err));
-    }
-
+process.on("uncaughtException", function (err) {
+  if (isVerboseOn) {
+    console.log(colors.red(err.stack));
+  } else {
+    console.log(colors.red(err));
+  }
 });
 
-var argv = yargs.usage('Usage: $0 -f [input JSON file] -o [output path for request body data file] -i [Elasticsearch index to write to] -t [Elasticsearch type to write]')
-    .demand(['f', 'i', 't'])
-    .alias('f', 'file')
-    .describe('f', 'Path to input JSON file')
-    .alias('o', 'output')
-    .describe('o', 'Output path of the request body data file')
-    .default('o', './')
-    .alias('i', 'index')
-    .describe('i', 'Elasticsearch index to write to')
-    .alias('t', 'type')
-    .describe('t', 'Name of the Elasticsearch type that is being inserted')
-    .alias('v', 'verbose')
-    .help('h')
-    .alias('h', 'help')
-    .epilog('Copyright 2015')
-    .argv;
+var argv = yargs
+  .usage(
+    "Usage: $0 -f [input JSON file] -o [output path for request body data file] -i [Elasticsearch index to write to] -t [Elasticsearch type to write]"
+  )
+  .demand(["f", "i", "t"])
+  .alias("f", "file")
+  .describe("f", "Path to input JSON file")
+  .alias("o", "output")
+  .describe("o", "Output path of the request body data file")
+  .default("o", "./")
+  .alias("i", "index")
+  .describe("i", "Elasticsearch index to write to")
+  .alias("t", "type")
+  .describe("t", "Name of the Elasticsearch type that is being inserted")
+  .alias("v", "verbose")
+  .help("h")
+  .alias("h", "help")
+  .epilog("Copyright 2015").argv;
 
-if(argv.verbose){
-    isVerboseOn = true;
+if (argv.verbose) {
+  isVerboseOn = true;
 }
 
 var stats = fs.statSync(argv.file);
-if(!stats.isFile()){
-    console.log(colors.red('Unable to find input file: ', argv.file));
-    exit(1);
+if (!stats.isFile()) {
+  console.log(colors.red("Unable to find input file: ", argv.file));
+  exit(1);
 }
 
 var inputJsonString = fs.readFileSync(argv.file),
-    inputJson;
+  inputJson;
 
-try{
-    inputJson = JSON.parse(inputJsonString);
-}
-catch(err){
-    console.log(colors.red('Unable to parse input json contents', err));
-    exit(1);
+try {
+  inputJson = JSON.parse(inputJsonString);
+} catch (err) {
+  console.log(colors.red("Unable to parse input json contents", err));
+  exit(1);
 }
 
 var outputStats = fs.statSync(argv.output);
-if(!outputStats.isDirectory()){
-    console.log(colors.red('[output] is not a valid directory: ', argv.output));
-    exit(1);
+if (!outputStats.isDirectory()) {
+  console.log(colors.red("[output] is not a valid directory: ", argv.output));
+  exit(1);
 }
 
-if(!_.isArray(inputJson)){
-    console.log(colors.red('Contents of JSON input file must be an array'));
-    exit(1);
+if (!_.isArray(inputJson)) {
+  console.log(colors.red("Contents of JSON input file must be an array"));
+  exit(1);
 }
 
-var stream = fs.createWriteStream(path.join(argv.output, 'request-data.txt'));
-stream.on('finish', function() {
-	console.log(colors.green('completed, wrote: ' + counter + ' record(s)'));
+var stream = fs.createWriteStream(
+  path.join(argv.output, path.parse(argv.file).name + ".txt")
+);
+stream.on("finish", function () {
+  console.log(colors.green("completed, wrote: " + counter + " record(s)"));
 });
 
-console.log(colors.gray('Writing records...'));
+console.log(colors.gray("Writing records..."));
 var counter = 0;
 
-stream.once('open', function(fd) {
+stream.once("open", function (fd) {
+  function stripChars(text) {
+    text = striptags(text);
+    text = text.replaceAll("&nbsp;", " ");
+    text = text.replaceAll("&lt;br&gt;", " ");
+    text = text.replaceAll("\r\n\r\n", " ");
+    text = text.replaceAll("\r\n", " ");
+    text = text.replaceAll("]", "'");
+    text = text.replaceAll("[", "'");
+    return text;
+  }
 
-    _.each(inputJson, function(record){
+  _.each(inputJson, function (record) {
+    var recordPrologue = { index: {} };
+    stream.write(JSON.stringify(recordPrologue) + "\n");
 
-        var recordPrologue = { index: { '_index': argv.index, '_id': record.id, '_type': argv.type } };
-        stream.write(JSON.stringify(recordPrologue) + '\n');
-        stream.write(JSON.stringify(record) + '\n');
+    // ---- Traitement spécifique aux portails, où le markup html est à éradiquer
+    //
+    record.DEMESSAGE = stripChars(record.DEMESSAGE);
+    //
+    record.REMESSAGE = stripChars(record.REMESSAGE);
+    //
+    record.POMESSAGE = stripChars(record.POMESSAGE);
+    //
+    stream.write(JSON.stringify(record) + "\n");
 
-        //process.stdout.write('.');
+    counter++;
+  });
 
-        if(counter % 100){
-           // console.log(colors.gray('.'));
-        }
-
-        counter++;
-    });
-
-    stream.end();
+  stream.end();
 });
