e121ee62dac4ba70689e3d94b22019f922ec3738
michelp76 <michel.puissant@inextenso.fr>
2025-01-16 09:30:27 +0100

    ajout commentaires

--- index.js
+++ index.js
@@ -99,6 +99,8 @@ stream.once("open", function (fd) {
     //
     record.POMESSAGE = stripChars(record.POMESSAGE);
     //
+    // --------------------------------------------------------------------------
+
     stream.write(JSON.stringify(record) + "\n");
 
     counter++;
